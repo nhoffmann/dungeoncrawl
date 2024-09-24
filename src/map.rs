286541dcc9a73da6b50d9720ab9e35c1865058bf
Niels Hoffmann <niels@zentralmaschine.net>
2024-09-24 11:40:29 +0200

    Add different map builders

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+pub const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 #[derive(Clone, Copy, PartialEq)]
 pub enum TileType {
